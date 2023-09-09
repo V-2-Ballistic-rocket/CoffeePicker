3def62aebe7cbd0f78033628396d1ec2615e03ef
V-2-Ballistic-rocket <sereeega2002@gmail.com>
2023-09-09 15:53:04 +0300

    Добавьте файлы проекта.

---
+++ Shared/Coffee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePicker.Shared
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string[] Ingredients { get; set; }
+
+        public Coffee() {}
+        public Coffee(int id, string Title, string Description, string[] Ingredients)
+        {
+            this.Id = id;
+
+            this.Title = Title;
+            this.Description = Description;
+            this.Ingredients = Ingredients;
+        }
+    }
+}
